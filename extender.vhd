10ff6dbd068fae6e0f80be2970a4ae2c800a31fd
allenzhong2015 <36616412+allenzhong2015@users.noreply.github.com>
2018-11-24 16:35:23 -0600

    Update extender.vhd

--- extender.vhd
+++ extender.vhd
@@ -1,23 +1,23 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use work.eecs361_gates.all;
-use work.eecs361.all;
 
-entity extender is 
+use work.eecs361_gates.all;
+
+entity extender is
 	port (
-		A	: in std_logic_vector(15 downto 0);
-		R	: out std_logic_vector(31 downto 0)
+		x	: in std_logic_vector(15 downto 0);
+		z	: out std_logic_vector(31 downto 0)
 	);
 end entity extender;
-	
+
 architecture structural of extender is
 
-begin 
+begin
 	lower_half : for i in 0 to 15 generate
-		R(i) <= A(i); 
-	end generate lower_half;
+		z(i) <= x(i);
+	end generate;
 	
 	upper_half : for i in 16 to 31 generate
-		R(i) <= A(15);
-	end generate upper_half;
-end structural;
+		z(i) <= x(15);
+	end generate;
+end architecture structural;
