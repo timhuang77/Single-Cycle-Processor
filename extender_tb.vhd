aa0a37bc7ad0d6580b3cf6479c17d4af04ca19bd
allenzhong2015 <36616412+allenzhong2015@users.noreply.github.com>
2018-11-24 16:34:56 -0600

    update extender testbench

--- extender_tb.vhd
+++ extender_tb.vhd
@@ -1,37 +1,33 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use work.eecs361_gates.all;
-use work.eecs361.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity extender_tb is
-	port (
-		z : out std_logic_vector(31 downto 0)
-	);
-end entity extender_tb;
+  port (
+    z       : out std_logic_vector(31 downto 0)
+  );
+end extender_tb;
 
 architecture structural of extender_tb is
 
-	component extender is 
-		port (
-			A	: in std_logic_vector(15 downto 0);
-			R	: out std_logic_vector(31 downto 0)
-		);
-	end component extender;
+component extender is
+  port (
+    x   : in  std_logic_vector(15 downto 0);
+    z   : out std_logic_vector(31 downto 0)
+  );
+end component extender;
 
-	signal x : std_logic_vector(15 downto 0);
+signal x : std_logic_vector(15 downto 0);
 
-	begin
-	
-		extender_map : extender port map (A => x, R => z);
-	
-		testbench : progress
-		begin
-		x <= "0000000000000011";
+begin
+  sign_extender_map : extender port map (x => x, z => z);
+  test_proc : process
+  begin
+    x <= "0000000000000011";
 
-		wait for 5 ns;
-
-		x <= "1111111111111100";
-		wait for 5 ns;
-		wait;
-	end process;
+    wait for 5 ns;
+   
+    x <= "1111111111111100";
+    wait for 5 ns;
+    wait;
+  end process;
 end architecture structural;
