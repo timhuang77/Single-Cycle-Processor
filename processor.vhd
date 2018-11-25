d767eaa3e280b8855c00364dca47a908c4402746
Tim Huang <tim81517lh@gmail.com>
2018-11-25 17:43:15 -0600

    added register and extender port maps, register port map incomplete (waiting on missing RegDst fix)

--- processor.vhd
+++ processor.vhd
@@ -11,12 +11,16 @@ entity processor is
 		-- clock
 		clk : in std_logic;
 		
+		-- Immediate Input Bus
+		imm16 : in std_logic_vector(15 downto 0);
+		
 		-- Register Addr Signals -- (5-bit addr)
 		Rs : in std_logic_vector(4 downto 0);
 		Rt : in std_logic_vector(4 downto 0);
 		Rd : in std_logic_vector(4 downto 0);
-		
+				
 		-- Control Signals --
+		nPC_sel : in std_logic;
 		ALU_ctrl : in std_logic_vector(3 downto 0);
 		RegDst : in std_logic;
 		RegWr : in std_logic;
@@ -57,6 +61,7 @@ signal DataMem_to_Mux_signal : std_logic_vector(31 downto 0);
 signal extender_to_Mux_signal : std_logic_vector(31 downto 0);
 
 begin
+	--Data Memory component
 	DataMemory : syncram 
 		generic map () 
 		port map(clk => clk, cs => '1', oe => '1', we => MemWrite, addr => ALU_to_Mux_signal, din => busB_signal, dout => DataMem_to_Mux_signal);
@@ -69,9 +74,30 @@ begin
 	MUX_RegToALU : mux_32 port map(ALU_src, busB_signal, extender_to_Mux_signal, RegMux_to_ALU_signal);
 	
 	--Register destination Mux
-	Mux_RegDst : mux_n 
-		generic map(n => 5)
-		port map(RegDst, Rt, Rd, Rw_signal);
+	-- Mux_RegDst : mux_n 
+		-- generic map(n => 5)
+		-- port map(RegDst, Rt, Rd, Rw_signal);
+		
+	--Extender component
+	Extender : extender port map(imm16, extender_to_Mux_signal);
+	
+	--Register 32
+	Register_file : register_component port map(
+		clk => clk, 
+		rst => '0',
+		we => RegWr,
+		busw => busW_signal,
+		rd => Rd,
+		rs => Rs,
+		rt => Rt,
+		busa => busA_signal,
+		busb => busB_signal
+	);
+		
+	
+		
+	
+	
 	
 	
 
