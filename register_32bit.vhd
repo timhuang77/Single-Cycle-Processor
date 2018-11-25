f01aad9c855ba4cf291426201fb5f35289c88c90
allenzhong2015 <36616412+allenzhong2015@users.noreply.github.com>
2018-11-25 15:41:38 -0600

    updated version

--- register_32bit.vhd
+++ register_32bit.vhd
@@ -4,30 +4,20 @@ use work.eecs361_gates.all;
 use work.eecs361.all;
 
 entity register_32bit is
-	port (
-		clk : in std_logic;
-		rst : in std_logic;
-		we : in std_logicl
-		d : in std_logic_vector(31 downto 0);
-		z : out std_logic_vector(31 downto 0)
-	);
-end register_32bit;
+  port (
+    arst, clk, enable   : in  std_logic;
+    d, aload       		: in  std_logic_vector(31 downto 0);
+	 q       				: out  std_logic_vector(31 downto 0)
+  );
+end entity register_32bit;
 
 architecture structural of register_32bit is
-	signal clk_temp, not_rst, we_temp : std_logic;
-	signal data_temp1, data_temp2, z_temp : std_logic_vector(31 downto 0);
-	
-	begin
-	
-		reset_wr : not_gate port map(rst, not_rst);
-		get_we	: or_gate port map (we, not_rst, we_temp);
-		
-		gen_ffs : for i in (0 to 31) generate
-			rst 		: and_gate port map(x => d(i), y => rst, z => data_temp1(i));
-			mux_we 	: mux port map(sel => we_temp, src0 => z_temp(i), src1 = data_temp1(i), z => data_temp2(i));
-			dffx 		: dff port map(clk => clk, d => data_temp2(i), q => z_temp(i));
-		end generate gen_ffs;
-		
-		z <= z_temp;
 
+begin
+
+  G0 : for i in 0 to 31 generate
+      dffr_map : dffr_a port map
+        (arst => arst, clk => clk, enable => enable, d => d(i), aload  => aload(i), adata => d(i), q => q(i));
+   end generate G0;
+  
 end architecture structural;
